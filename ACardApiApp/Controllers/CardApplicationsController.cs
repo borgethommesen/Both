7734b5f457c673f13136f4946ec3208bbedd8d69
borgethommesen <borge.thommesen@gmail.com>
2015-09-03 16:21:57 +0200

    Initial

---
+++ ACardApiApp/Controllers/CardApplicationsController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using CardApplications.Models;
+using ACardApiApp.Models;
+
+namespace CardApplications.Controllers
+{
+    public class CardApplicationController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<CardApplication> Get()
+        {
+            using (var db = new ACardApiAppContext())
+            {
+                var query = from b in db.CardApplications
+                            where b.Status == false
+                            orderby b.CardName
+                            select b;
+
+                if (query.Count() <= 0)
+                {
+                    //Returnera en tom objekt i de fall det inte finns några 
+                    //Cardapplications att föra över
+                    var CardApplicationObjects = new CardApplication[1];
+                    var application = new CardApplication();
+                    CardApplicationObjects[0] = application;
+                    return CardApplicationObjects;
+                }
+                else
+                {
+                    var CardApplicationObjects = new CardApplication[query.Count()];
+                    int i = 0;
+                    foreach (var item in query)
+                    {
+                        if (item.Status == false)
+                        {
+                            var application = new CardApplication();
+                            application.ID = item.ID;
+                            application.AccountAgreement = item.AccountAgreement;
+                            application.Address = item.Address;
+                            application.ApprovedAmount = item.ApprovedAmount;
+                            application.CoAddress = item.CoAddress;
+                            application.Country = item.Country;
+                            application.Currency = item.Currency;
+                            application.DateOfBirth = item.DateOfBirth;
+                            application.CardName = item.CardName;
+                            application.FirstName = item.FirstName;
+                            application.SurName = item.SurName;
+                            application.Gender = item.Gender;
+                            application.HandlingOfficer = item.HandlingOfficer;
+                            application.Language = item.Language;
+                            application.Office = item.Office;
+                            application.PostalCity = item.PostalCity;
+                            application.PostalCode = item.PostalCode;
+                            application.ProductID = item.ProductID;
+                            application.SocialSecurityNumber = item.SocialSecurityNumber;
+                            application.SpendingLimit = item.SpendingLimit;
+
+                            CardApplicationObjects[i] = application;
+                            i++;
+
+                            // updatera status= true=överförd, false=ej överförd
+                            var ApplicationStatus = db.CardApplications.Find(item.ID);
+                            ApplicationStatus.Status = true;
+                        }
+                    }
+                    db.SaveChanges();
+                    return CardApplicationObjects;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ ACardApiApp/Models/CardApplication.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CardApplications.Models
+{
+    public class CardApplication
+    {
+
+    public int ID {get; set;}
+    public string FirstName { get; set; }
+    public string SurName { get; set; }
+    public string CardName { get; set; }
+    public string Gender { get; set; }
+    public string DateOfBirth { get; set; }
+    public string SocialSecurityNumber { get; set; }
+    public string Address { get; set; }
+    public string CoAddress { get; set; }
+    public string PostalCode { get; set; }
+    public string PostalCity { get; set; }
+    public string Country { get; set; }
+    public string Language { get; set; }
+    public string ProductID { get; set; }
+    public string AccountAgreement { get; set; }
+    public string Currency { get; set; }
+    public string ApprovedAmount { get; set; }
+    public double SpendingLimit { get; set; }
+    public string Office { get; set; }
+    public string HandlingOfficer { get; set; }
+    public Boolean Status { get; set; }  
+    }
+
+
+}
